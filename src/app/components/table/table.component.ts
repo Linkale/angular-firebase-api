04930faa64051011dead700f450e0ba272d6586d
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2023-05-09 12:14:48 +0200

    modify table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
+import { Router } from '@angular/router';
 import { BehaviorSubject, combineLatest, filter, map, Observable, switchMap, tap } from 'rxjs';
 import { PandascoreService } from 'src/app/services/pandascore.service';
 import { Match } from '../models/interfaces';
@@ -17,14 +18,14 @@ export class TableComponent {
   count: number = 0;
   tableSize: number = 10;
   tableSizes: number[] = [5, 10, 15, 20];
-  displayedColumns: string[] = ['name', 'tournament', 'status'];
+  displayedColumns: string[] = ['date', 'name', 'league', 'stage', 'status'];
 
   @Input()
   set titleSpecial(titleSpecial: string) {
     this.type.next(titleSpecial)
   }
   
-  constructor(private pandascore: PandascoreService) {
+  constructor(private pandascore: PandascoreService, private router: Router) {
     this.data = combineLatest([this.page, this.type])
     .pipe(
       filter(([_,t]) => t !== undefined), 
@@ -36,8 +37,12 @@ export class TableComponent {
   ngOnInit(): void { 
   }
 
-  onTableDataChange(event: PageEvent) {
+  onTableDataChange(event: PageEvent): void {
     this.page.next(event.pageIndex);
+  }
+
+  async goToMatch(idMatch: number) { 
+    await this.router.navigateByUrl('/match/'+idMatch);
   }
 
   // onTableSizeChange(event: any): void {
